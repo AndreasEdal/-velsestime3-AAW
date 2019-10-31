641f7b04856598e4262c47f50efe7af8b97be472
William Bolding <w.bolding@outlook.com>
2019-10-31 08:59:03 +0100

    Oprettet noget.

---
+++ src/projekt/CorruptedFile.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projekt;
+
+/**
+ *
+ * @author wbold
+ */
+public class CorruptedFile {
+    public void printCorruption(){
+        System.out.println("ERROR File is Corrupted");
+    }
+}
--- src/projekt/Projekt.java
+++ src/projekt/Projekt.java
@@ -15,7 +15,8 @@ public class Projekt {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        CorruptedFile cf = new CorruptedFile();
+        cf.printCorruption();
     }
     
 }
